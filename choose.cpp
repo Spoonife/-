dfcb707a25adf42d7216f09b9ba5117a1a5e564e
Spoonife <65659130+Spoonife@users.noreply.github.com>
2021-07-11 16:25:26 +0800

    Create choose.cpp

---
+++ choose.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<iostream>
+
+constexpr std::uint32_t hash_str_to_uint32(const char* data)
+{
+    std::uint32_t h(0);
+    for (int i = 0; data && ('\0' != data[i]); i++)
+        h = (h << 6) ^ (h >> 26) ^ data[i];
+    return h;
+}
